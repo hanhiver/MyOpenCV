4d41f7f88676430b059da17a9786dd7f623b0365
hanhiver <13910395157@139.com>
2019-05-07 16:55:23 +0800

    update

--- contours/findCountours.py
+++ contours/findCountours.py
@@ -82,6 +82,84 @@ def phaseVideo(input_path, output_path = None):
                 sample_file = 'sample.bmp'
                 cv2.imwrite(sample_file, frame)
 
+
+
+def phaseVideo2(input_path, output_path = None):
+    camera = cv2.VideoCapture(input_path)
+    if camera is None:
+        print('Open file: {} failed. '.format(input_path))
+        return
+
+    if output_path != None:
+        video_size = (int(camera.get(cv2.CAP_PROP_FRAME_WIDTH)),
+                        int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+        video_FourCC = cv2.VideoWriter_fourcc(*'XVID')
+        out = cv2.VideoWriter(output_path, video_FourCC, 25, video_size)
+
+    pre_frame = None
+    display = None
+
+    cv2.namedWindow('Result', flags = cv2.WINDOW_NORMAL)
+
+    pre_frame = cv2.imread('mysample.jpg')
+    pre_frame = cv2.cvtColor(pre_frame, cv2.COLOR_RGB2GRAY)
+
+    while True:
+        (res, frame) = camera.read()
+        if res != True:
+            break
+
+        image_gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+        image_gray = cv2.GaussianBlur(image_gray, (21, 21), 0)
+        image_curr = image_gray
+
+        image_delta = cv2.absdiff(pre_frame, image_curr)
+        thresh = cv2.threshold(image_gray, 80, 255, cv2.THRESH_BINARY)[1]
+        thresh = cv2.dilate(thresh, None, iterations = 2)
+        contours, hierarchy = cv2.findContours(thresh.copy(), 
+                                           cv2.RETR_EXTERNAL, 
+                                           cv2.CHAIN_APPROX_SIMPLE)
+        """
+        contours_valid = []
+        for item in contours:
+            if cv2.contourArea(item) > 300:
+                rect = cv2.boundingRect(item)
+                #print('Contour:', item)
+                #print('Rect: ', rect)
+                contours_valid.append(rect)
+                #image_delta = cv2.rectangle(image_delta, 
+                #image_delta = cv2.rectangle(frame,     
+                #                            (rect[0], rect[1]), 
+                #                            (rect[0] + rect[2], rect[1] + rect[3]), 
+                #                            (255, 255, 255), 2)
+        """
+
+        image_contours = cv2.drawContours(image_gray, contours, -1, (255, 255, 255), 1)
+        #image_contours = image_delta
+
+        #print('Size of contours: ', len(contours))
+        #for item in contours:
+        #    print('   ==> countour size: ', cv2.contourArea(item))
+
+        display = np.hstack([image_gray, image_contours])
+        #display = frame
+        
+        # Already set the sample image in the begining. 
+        #pre_frame = image_curr
+
+        if output_path != None:
+            out.write(display)
+
+        cv2.imshow('Result', display)
+        key = cv2.waitKey(1)
+        if key & 0xFF == ord('q'):
+            return
+        elif key & 0xFF == ord('s'):
+            #sample_file = input_path.split('.')[0] + '_sample.jpg'
+            sample_file = 'sample.bmp'
+            cv2.imwrite(sample_file, frame)
+
+
 # 像素流动方式检测
 def phaseVideo3(input_path, output_path = None):
     camera = cv2.VideoCapture(input_path)
@@ -230,8 +308,8 @@ def phaseVideo4(input_path, output_path = None):
 # 滑动窗口法
 def phaseVideo4(input_path, output_path = None):
 
-    QUEUE_LENGTH = 20
-    WINDOWS_SIZE = 10
+    QUEUE_LENGTH = 40
+    WINDOWS_SIZE = 20
 
     camera = cv2.VideoCapture(input_path)
     if camera is None:
@@ -294,7 +372,14 @@ def phaseVideo4(input_path, output_path = None):
             sample_file = 'sample.bmp'
             cv2.imwrite(sample_file, frame)
 
-def phaseVideo2(input_path, output_path = None):
+
+
+# 滑动窗口比对三连张法。
+def phaseVideo5(input_path, output_path = None):
+
+    QUEUE_LENGTH = 500
+    acum_frame = None
+    
     camera = cv2.VideoCapture(input_path)
     if camera is None:
         print('Open file: {} failed. '.format(input_path))
@@ -306,52 +391,66 @@ def phaseVideo2(input_path, output_path = None):
         video_FourCC = cv2.VideoWriter_fourcc(*'XVID')
         out = cv2.VideoWriter(output_path, video_FourCC, 25, video_size)
 
-    pre_frame = None
+    frame_index = 0
     display = None
+    frame_queue = []
 
     cv2.namedWindow('Result', flags = cv2.WINDOW_NORMAL)
 
-    pre_frame = cv2.imread('mysample.jpg')
-    pre_frame = cv2.cvtColor(pre_frame, cv2.COLOR_RGB2GRAY)
+    #pre_frame = cv2.imread('mysample.jpg')
+    #pre_frame = cv2.cvtColor(pre_frame, cv2.COLOR_RGB2GRAY)
 
     while True:
         (res, frame) = camera.read()
         if res != True:
             break
 
+        frame_index += 1
+        (h, w) = frame.shape[:2]
+        frame = cv2.resize(frame, (w//2, h//2), interpolation = cv2.INTER_LINEAR)
         image_gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-        image_gray = cv2.GaussianBlur(image_gray, (21, 21), 0)
-        image_curr = image_gray
 
-        image_delta = cv2.absdiff(pre_frame, image_curr)
-        thresh = cv2.threshold(image_gray, 80, 255, cv2.THRESH_BINARY)[1]
+        image_to_queue = np.array(image_gray, dtype = np.uint16)
+        frame_queue.append(image_to_queue)
+
+
+
+        if acum_frame is None: 
+            acum_frame = np.array(image_gray, dtype = np.uint64)
+            continue
+
+        acum_frame += image_gray
+
+        if len(frame_queue) < QUEUE_LENGTH - 1:
+            continue
+
+        image_avg = acum_frame//QUEUE_LENGTH
+        image_avg = np.array(image_avg, dtype = np.uint8)
+        
+        old_frame = frame_queue.pop(0)
+        acum_frame -= old_frame
+
+        blur_image = cv2.GaussianBlur(image_gray, (21, 21), 0)
+
+        image_delta = cv2.absdiff(image_avg, blur_image)
+        
+        thresh = cv2.threshold(image_delta, 60, 255, cv2.THRESH_BINARY)[1]
         thresh = cv2.dilate(thresh, None, iterations = 2)
         contours, hierarchy = cv2.findContours(thresh.copy(), 
                                            cv2.RETR_EXTERNAL, 
                                            cv2.CHAIN_APPROX_SIMPLE)
-        """
+
         contours_valid = []
         for item in contours:
-            if cv2.contourArea(item) > 300:
-                rect = cv2.boundingRect(item)
-                #print('Contour:', item)
-                #print('Rect: ', rect)
-                contours_valid.append(rect)
-                #image_delta = cv2.rectangle(image_delta, 
-                #image_delta = cv2.rectangle(frame,     
-                #                            (rect[0], rect[1]), 
-                #                            (rect[0] + rect[2], rect[1] + rect[3]), 
-                #                            (255, 255, 255), 2)
-        """
+            area = cv2.contourArea(item)
+            if area > 1500 and area < 30000:
+                contours_valid.append(item)
+                print(area)
 
-        image_contours = cv2.drawContours(image_gray, contours, -1, (255, 255, 255), 1)
-        #image_contours = image_delta
+        image_contours = cv2.drawContours(image_avg//2, contours_valid, -1, (255, 255, 255), -1)
 
-        #print('Size of contours: ', len(contours))
-        #for item in contours:
-        #    print('   ==> countour size: ', cv2.contourArea(item))
-
-        display = np.hstack([image_gray, image_contours])
+        display = np.hstack([image_gray, image_avg, image_contours])
+        #display = np.hstack([image_gray, image_contours])
         #display = frame
         
         # Already set the sample image in the begining. 
@@ -369,6 +468,106 @@ def phaseVideo2(input_path, output_path = None):
             sample_file = 'sample.bmp'
             cv2.imwrite(sample_file, frame)
 
+
+
+# 大量照片累计干扰消除法。
+def phaseVideo6(input_path, output_path = None):
+
+    acum_frame = None
+    ACUM_NUMBER = 100
+    
+    camera = cv2.VideoCapture(input_path)
+    if camera is None:
+        print('Open file: {} failed. '.format(input_path))
+        return
+
+    if output_path != None:
+        video_size = (1920, 1080)
+        video_fps = camera.get(cv2.CAP_PROP_FPS)
+        video_FourCC = cv2.VideoWriter_fourcc(*'x264')
+        #video_FourCC = int(camera.get(cv2.CAP_PROP_FOURCC))
+        print('OUT: ', video_FourCC, video_fps, video_size)
+        out = cv2.VideoWriter(output_path, video_FourCC, video_fps, video_size, False)
+
+    frame_index = 0
+    display = None
+
+    cv2.namedWindow('Result', flags = cv2.WINDOW_NORMAL)
+
+    #pre_frame = cv2.imread('mysample.jpg')
+    #pre_frame = cv2.cvtColor(pre_frame, cv2.COLOR_RGB2GRAY)
+
+    while True:
+        (res, frame) = camera.read()
+        if res != True:
+            break
+
+        frame_index += 1
+        (h, w) = frame.shape[:2]
+        frame = cv2.resize(frame, (w//2, h//2), interpolation = cv2.INTER_LINEAR)
+        image_gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+
+        if acum_frame is None: 
+            acum_frame = np.array(image_gray, dtype = np.uint64)
+            continue
+
+        acum_frame += image_gray
+
+        if frame_index < ACUM_NUMBER:
+            continue
+
+        image_avg = acum_frame//frame_index
+        image_avg = np.array(image_avg, dtype = np.uint8)
+
+        if frame_index >= 30000:
+            acum_frame = acum_frame // 2
+            frame_index = frame_index // 2
+        
+        #blur_image = cv2.GaussianBlur(image_gray, (21, 21), 0)
+        blur_image = image_gray
+
+        image_delta = cv2.absdiff(image_avg, blur_image)
+        
+        thresh = cv2.threshold(image_delta, 50, 255, cv2.THRESH_BINARY)[1]
+        #thresh = cv2.adaptiveThreshold(image_delta, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
+        thresh = cv2.dilate(thresh, None, iterations = 2)
+        contours, hierarchy = cv2.findContours(thresh.copy(), 
+                                           cv2.RETR_EXTERNAL, 
+                                           cv2.CHAIN_APPROX_SIMPLE)
+
+        contours_valid = []
+        for item in contours:
+            area = cv2.contourArea(item)
+            if area > 100 and area < 50000:
+                contours_valid.append(item)
+                #print(area)
+
+        #print('Frame: ', frame_index, ' Areas: ', len(contours_valid))
+        image_contours = cv2.drawContours(image_gray//2, contours_valid, -1, (255, 255, 255), -1)
+
+        image1 = np.hstack([image_gray, image_avg])
+        image2 = np.hstack([thresh, image_contours])
+        display = np.vstack([image1, image2])
+        #display = cv2.cvtColor(display, cv2.COLOR_GRAY2RGB)
+        #print('SIZE: ', display.shape)
+        #display = np.hstack([image_gray, image_avg, thresh])
+        #display = np.hstack([image_gray, image_contours])
+        #display = frame
+        
+        # Already set the sample image in the begining. 
+        #pre_frame = image_curr
+
+        if output_path != None:
+            out.write(display)
+
+        cv2.imshow('Result', display)
+        key = cv2.waitKey(1)
+        if key & 0xFF == ord('q'):
+            return
+        elif key & 0xFF == ord('s'):
+            #sample_file = input_path.split('.')[0] + '_sample.jpg'
+            sample_file = 'sample.bmp'
+            cv2.imwrite(sample_file, frame)
 
 
 def main():
@@ -392,7 +591,7 @@ def main():
     if 'input' in FLAGS:
         
         # detect_video(FLAGS.input,  
-        phaseVideo4(input_path = FLAGS.input,  
+        phaseVideo6(input_path = FLAGS.input,  
                    output_path = FLAGS.output)
 
     else:
