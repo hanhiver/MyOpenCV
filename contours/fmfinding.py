af63db3e328a5cdde489254b794024b318cae9be
hanhiver <13910395157@139.com>
2019-06-06 11:58:55 +0800

    update

--- contours/fmfinding.py
+++ contours/fmfinding.py
@@ -2,20 +2,22 @@ import cv2
 import numpy as np 
 import time
 
-# Class to find the foreign matter in the highway. 
+# Class to find the foreign matters in the highway. 
 class FMFinding():
 
-	def __init__(self, width = 960, height = 540):
+	def __init__(self, width = 960, height = 540, accu_number = 30000, fm_threshold = 50):
 		self.frame_index = 0
 		self.w = width
 		self.h = height
+		self.accu_number = 30000 # 25 frames per second, default 20 minutes. 
+		self.fm_threshold = 50 # threshold of the foreign matters that diff with the background. 	
 
 		#self.curr_frame = None
 		self.acum_frame = None
 		self.acum_contours = None
 		self.acum_contours_base = None
 
-		self.ACCUM_NUMBER = 100
+
 
 	def init_acum_frame(self):
 		self.acum_frame = np.zeros(shape = (self.h, self.w), dtype = np.uint32)
@@ -36,7 +38,7 @@ class FMFinding():
 
 		self.frame_avg = np.array((self.acum_frame // self.frame_index), dtype = np.uint8)
 
-		if self.frame_index >= 30000:
+		if self.frame_index >= self.accu_number:
 			self.acum_frame = self.acum_frame // 2
 			self.frame_index = self.frame_index // 2
 
@@ -46,7 +48,7 @@ class FMFinding():
 		frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 		frame_delta = cv2.absdiff(self.frame_avg, frame)
 
-		thresh = cv2.threshold(frame_delta, 50, 255, cv2.THRESH_BINARY)[1]
+		thresh = cv2.threshold(frame_delta, self.fm_threshold, 255, cv2.THRESH_BINARY)[1]
 		thresh = cv2.dilate(thresh, None, iterations = 2)
 
 		if cv2.__version__[0] == '3':
@@ -85,6 +87,10 @@ class FMFinding():
 	# Return the accumulate frame which has no moving objects. 
 	def get_standard_frame(self):
 		return self.frame_avg
+
+	# Return the previous frame that added. (already transfer to gray.) 
+	def get_gray_frame(self):
+		return self.frame_gray
 
 	# Return a frame that mark the contours in current input frame. 
 	def get_contours_frame(self):
