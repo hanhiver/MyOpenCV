06083afa10b5b6b421d08457e59960d8364193f2
Dong Han <handongfr@gmail.com>
2019-07-23 15:00:58 +0800

    update readCam cpp program.

---
+++ readCam/CMakeFiles/readCam_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/readCam_cpp.dir/readCam_cpp.cpp.o"
+  "readCam_cpp.pdb"
+  "readCam_cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/readCam_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ readCam/readCam_cpp.cpp
@@ -0,0 +1,43 @@
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+using namespace cv;
+
+#include <iostream>
+#include <ctime>
+
+clock_t  start, end; 
+
+int main()
+{
+	start = clock();
+	//VideoCapture cap("./1s.mp4");
+	VideoCapture cap(0);
+
+	if (!cap.isOpened())
+	{
+		std::cerr << "Open Video Error!" << std::endl;
+	}
+
+	Mat srcImg, grayImg;
+
+	namedWindow("DEMO", 1);
+	
+	std::cout << "Initialized in " << (float)(clock() - start) * 1000.0 / CLOCKS_PER_SEC << " ms." << std::endl;
+	start = clock();
+
+	while (cap.read(srcImg))
+	{
+		std::cout << "Got one frame in  " << (float)(clock() - start) * 1000.0 / CLOCKS_PER_SEC << " ms."  << std::endl;
+		start = clock();
+
+		imshow("DEMO", srcImg);
+		std::cout << "Show one frame in " << (float)(clock() - start) * 1000.0 / CLOCKS_PER_SEC << " ms."  << std::endl;
+		
+		waitKey(1);
+		start = clock();
+	}
+
+	return 0;
+}
